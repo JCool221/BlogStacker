8e80029dbe4a1352696d9469d914b29b92990ea5
Jcool221 <jcool2939@gmail.com>
2023-01-11 14:52:48 -0500

    route updates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -67,14 +67,63 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', async (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
+  } try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+      order:[['date_created', 'DESC']],
+      limit: 5,
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // Pass serialized data and session flag into template
+    res.render('login', {posts,});
+  } catch (err) {
+    res.status(500).json(err);
   }
 
-  res.render('login');
 });
+
+router.get('/signup', async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }   try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+      order:[['date_created', 'DESC']],
+      limit: 5,
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // Pass serialized data and session flag into template
+
+
+  res.render('signup', {posts,});
+  } catch (err) {
+  res.status(500).json(err);
+  }
+
+});
+
 
 module.exports = router;
